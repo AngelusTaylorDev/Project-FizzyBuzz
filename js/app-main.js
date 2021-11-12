1d4326ab44d5db0d4433d8555b16a7f431acc56f
Angelus Taylor <angelustaylor@outlook.com>
2021-11-12 09:54:31 -0600

    The Initial setup for Fuzzy Buzz
    Used the template I created to create the initial setup for Fuzzy Buzz

---
+++ js/app-main.js
@@ -0,0 +1,9 @@
+// Pseudo code
+// --------------------------------------------------
+// 1. Sellect the HTML elements needed 
+// 2. Get the user input from the string they provide.
+// 3. Loop through and reverse the string 
+// 4. Run the Program after the button is pushed
+// 5. Display the string in the alert on the page
+// --------------------------------------------------
+
